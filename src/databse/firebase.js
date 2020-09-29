218c576489bd09174ffeffabe603693d970d2abd
sebv <sebv@sv.home>
2020-09-29 19:21:12 +0200

    update

--- src/databse/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyBQOt28__1aaFdwrsLgqG9c1E8jAF69Zg4',
-  authDomain: 'clone-69adf.firebaseapp.com',
-  databaseURL: 'https://clone-69adf.firebaseio.com',
-  projectId: 'clone-69adf',
-  storageBucket: 'clone-69adf.appspot.com',
-  messagingSenderId: '781661640375',
-  appId: '1:781661640375:web:b59f2563faf9a1ada91f32',
-};
